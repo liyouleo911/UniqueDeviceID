6e5ead70958626bf593748eed102f26c542f9d64
Liyou <liyouleo911@msn.com>
2021-12-15 15:47:11 +0800

    Initial commit

--- Sources/UniqueDeviceID/UniqueDeviceID.swift
+++ Sources/UniqueDeviceID/UniqueDeviceID.swift
@@ -1,6 +1,46 @@
-public struct UniqueDeviceID {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import SwiftKeychainWrapper
 
-    public init() {
+public class UniqueDeviceID {
+    
+    private static let udidKey = "UniqueDeviceID_UDID_Key"
+    
+    /// Retrieve the device's identifier
+    /// - Returns: Self generated UDID
+    public static func udid() -> String {
+        uuid(for: udidKey, isCachedToLocal: true)
+    }
+
+    // MARK: private
+    private static func uuid(for key: String, isCachedToLocal: Bool = true) -> String {
+        let resultUUID: String
+        if let uuidFromKeyChain = uuidFromKeyChain(for: key) {
+            resultUUID = uuidFromKeyChain
+        } else {
+            if isCachedToLocal, let uuidFromLocalCache = uuidFromLocalCache(for: key) {
+                resultUUID = uuidFromLocalCache
+            } else {
+                resultUUID = UUID().uuidString
+            }
+            KeychainWrapper.standard.set(resultUUID, forKey: key)
+        }
+        if isCachedToLocal {
+            UserDefaults.standard.set(resultUUID, forKey: key)
+        }
+        return resultUUID
+    }
+    
+    private static func uuidFromKeyChain(for key: String) -> String? {
+        if let uuidString = KeychainWrapper.standard.string(forKey: key), let _ = UUID(uuidString: uuidString) {
+            return uuidString
+        }
+        return nil
+    }
+    
+    private static func uuidFromLocalCache(for key: String) -> String? {
+        if let uuidString = UserDefaults.standard.string(forKey: key), let _ = UUID(uuidString: uuidString) {
+            return uuidString
+        }
+        return nil
     }
 }
--- Tests/UniqueDeviceIDTests/UniqueDeviceIDTests.swift
+++ Tests/UniqueDeviceIDTests/UniqueDeviceIDTests.swift
@@ -6,6 +6,6 @@ final class UniqueDeviceIDTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(UniqueDeviceID().text, "Hello, World!")
+//        XCTAssertEqual(UniqueDeviceID().text, "Hello, World!")
     }
 }
